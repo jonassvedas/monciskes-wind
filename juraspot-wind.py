94749d189187966e87e7dc7337f5fbc9d9f585f7
Jonas Svedas <jsvedas@gmail.com>
2021-08-25 17:03:22 +0300

    added check for wind conditions

--- juraspot-wind.py
+++ juraspot-wind.py
@@ -1,14 +1,38 @@
+#! python3
+
 import requests
 import pytesseract
 # import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
+
+def is_angle_ok(angle, start, end):
+    if start >= end:
+        if (angle >= start) and (angle <= end):
+            return True
+    else:
+        if not((angle >= end) and (angle <= start)):
+            return True
+    return False
+
+def is_speed_ok(speed, min_speed, max_speed):
+    if (speed >= min_speed) and (speed <= max_speed):
+        return True
+    else:
+        return False
+
+
 avg_wind_url = "http://juraspot.lt/images/10MinAvgWindSpeed.php"
 dir_wind_url = "http://juraspot.lt/images/WindDirection.php"
 
 avg_wind_img = "avg_wind.png"
 dir_wind_img = "dir_wind.png"
+
+wind_min, wind_max = (6.5, 13.5)
+# for the angle settings the vector (1;0) is assumed to be at 0 deg, angle is increasing anticlockwise.
+# angle is from 0 to 359
+dir_start, dir_end = (87, 273)
 
 img = requests.get(avg_wind_url).content
 with open(avg_wind_img, 'wb') as handler:
@@ -20,7 +44,7 @@ with open(dir_wind_img, 'wb') as handler:
 
 # process wind speed
 speed_data = pytesseract.image_to_string(Image.open(avg_wind_img)).split()
-speed = speed_data[5]
+speed = float(speed_data[5])
 date = speed_data[6]
 time = speed_data[7]
 
@@ -46,9 +70,15 @@ red_avg = np.mean(red_coordinates, axis = 0)
 # Normalize and invese y axis
 norm = (center - red_avg) * np.array([1,-1])
 
-angle = int(np.arctan2(norm[0], norm[1]) / 3.1416 * 180)
+angle = np.arctan2(norm[0], norm[1] / 3.1416 * 180)
+angle = int(angle + 360 if angle < 0 else angle) % 360
 
 # plt.imshow(red_pixels)
 # plt.show()
 
 print(angle, "deg |", speed, "m/s |", date, time)
+if (is_angle_ok(angle, dir_start, dir_end) and is_speed_ok(speed, wind_min, wind_max)):
+    print("OK to kiteboard.")
+else:
+    print("NOT OK to kiteboard.")
+
